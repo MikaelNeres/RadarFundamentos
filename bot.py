da449aba08a4c02888387fa80f4e05a89df4bd16
MikaelNeres <mikaelneres@gmail.com>
2026-09-15 16:41:04 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,8 +25,8 @@ logger = logging.getLogger(__name__)
 # ==============================================================================
 # CONFIGURAÇÕES GERAIS
 # ==============================================================================
-TOKEN = os.getenv('8734276492:AAGR92m7XYBWo_Ac5SHvbVBQL9K40ErIsrE')
-CHAT_ID = os.getenv('566929604')
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 if not TOKEN or not CHAT_ID:
     logger.error("❌ TOKEN ou CHAT_ID não configurados!")
@@ -50,8 +50,6 @@ MEUS_ATIVOS = [
     "VBBR3",
     "SAUD3",
     "DEXP3",
-    "VALE3",
-    "PETR4",
     # Adicione novos tickers aqui (ex: "PETR4", "VALE3")
 ]
 
